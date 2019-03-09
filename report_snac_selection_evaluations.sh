4d3055839936e86b7869b77536d2525af27597e7
jteoh <jiateoh@gmail.com>
2019-03-09 07:29:43 -0800

    Uploading Jason's evaluation results (including program killed instances) for all NC/SNAC evaluations for MNIST/Driving/PDF, except NC-Driving (in progress)

--- report_snac_selection_evaluations.sh
+++ report_snac_selection_evaluations.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-# for each iteration, prints DX-NC and True-NC (name??). Note that True-NC calculates individual neuron coverage based on the unscaled activation output.
 mkdir -p official_report_results
 
+# comment/uncomment lines as needed.
+SELECTION="nc" # or snac
 # MNIST
-echo "Warning: only running MNIST right now (we still need to update Driving + PDF with appropriate nc metrics)"
-(cd MNIST && python gen_diff.py light 1 0.1 10 500 20 0) | tee official_report_results/mnist_500_snac_selection_light.log
-(cd MNIST && python gen_diff.py occl 1 0.1 10 500 20 0) | tee official_report_results/mnist_500_snac_selection_occl.log
-(cd MNIST && python gen_diff.py blackout 1 0.1 10 500 20 0) | tee official_report_results/mnist_500_snac_selection_blackout.log
+# (cd MNIST && python gen_diff.py light 1 0.1 10 500 20 0 $SELECTION) | tee official_report_results/mnist_500_${SELECTION}_selection_light.log
+# (cd MNIST && python gen_diff.py occl 1 0.1 10 500 20 0 $SELECTION) | tee official_report_results/mnist_500_${SELECTION}_selection_occl.log
+# (cd MNIST && python gen_diff.py blackout 1 0.1 10 500 20 0 $SELECTION) | tee official_report_results/mnist_500_${SELECTION}_selection_blackout.log
 
 # # Driving
-# (cd Driving && python gen_diff.py light 1 0.1 10 500 20 0) | tee official_report_results/driving_500_snac_selection_light.log
-# (cd Driving && python gen_diff.py occl 1 0.1 10 500 20 0) | tee official_report_results/driving_500_snac_selection_occl.log
-# (cd Driving && python gen_diff.py blackout 1 0.1 10 500 20 0) | tee official_report_results/driving_500_snac_selection_blackout.log 
+# (cd Driving && python gen_diff.py light 1 0.1 10 500 20 0 $SELECTION) | tee official_report_results/driving_500_${SELECTION}_selection_light.log
+# (cd Driving && python gen_diff.py occl 1 0.1 10 500 20 0 $SELECTION) | tee official_report_results/driving_500_${SELECTION}_selection_occl.log
+# (cd Driving && python gen_diff.py blackout 1 0.1 10 500 20 0 $SELECTION) | tee official_report_results/driving_500_${SELECTION}_selection_blackout.log 
 
 # # PDF
-# (cd PDF && python gen_diff.py 2 0.1 0.1 500 20 0) | tee official_report_results/pdf_500_snac_selection.log
+(cd PDF && python gen_diff.py 2 0.1 0.1 500 20 0 $SELECTION) | tee official_report_results/pdf_500_${SELECTION}_selection.log
